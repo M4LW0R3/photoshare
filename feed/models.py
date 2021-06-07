cef312f354bcbd7b93ed721c189db3c60162e5c9
M4LW0R3 <youngkhalifah98@gmail.com>
2021-06-07 13:39:06 +0300

    initial commit

---
+++ feed/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from autoslug import AutoSlugField
+
+# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile_pics')
